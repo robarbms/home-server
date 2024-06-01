775c96de362a30054b43cb09144ebecfc7b65d9b
robarbms <robjamesbarber@gmail.com>
2024-05-31 17:15:03 -0700

    Adding Ollama

--- src/components/tiles/AI.tsx
+++ src/components/tiles/AI.tsx
@@ -5,10 +5,10 @@ export default function AI(props) {
     return(
         <Card>
             <h2>AI Services</h2>
-            <h3>Stable Diffusion WebUI</h3>
-            <p></p>
-            <h3>ComfyUI</h3>
-            <p></p>
+            <h3>Text Generation</h3>
+            <a href="http://home:3000" className="button" target="_blank">Ollama - WebUI</a>
+            <h3>Image Generation</h3>
+            <a href="http://home:8188" className="button" target="_blank">ComfyUI</a>
             <h3>Resources</h3>
         </Card>
     )
