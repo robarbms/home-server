92e327d1e3e07f6c464f72237ec8e300ceeab746
robarbms <robjamesbarber@gmail.com>
2024-06-22 15:23:27 -0700

    Adding content

--- src/components/tiles/AI.tsx
+++ src/components/tiles/AI.tsx
@@ -12,10 +12,11 @@ export default function AI() {
         <Card>
             <h2>AI Services</h2>
             <h3>Text Generation</h3>
-            <a href="http://home:3000" className="button" target="_blank">Ollama - WebUI</a>
+            <a href="http://home:42421" className="button" target="_blank">Ollama - WebUI</a>
             <h3>Image Generation</h3>
             <a href="http://home:8188" className="button" target="_blank">ComfyUI</a>
             <h3>Resources</h3>
+            <a href="http://home:42000" className="button" target="_blank">Pinokio</a>
         </Card>
     )
 }
